2939879e23ee3c2db5d6a18f216be40ba7d9e3ba
kasamax <kqss.251@gmail.com>
2022-04-04 22:48:38 +0900

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Aimi on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
